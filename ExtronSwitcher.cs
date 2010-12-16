709f252681eed0046b319f7647143de7d927be88
Kaung Myat <kaungmo@gmail.com>
2010-12-16 13:42:54 +0800

    This is from Ebook

--- ExtronSwitcher.cs
+++ ExtronSwitcher.cs
@@ -10,6 +10,7 @@ namespace Extron
     class ExtronSwitcher
     {
         //This is V2 Branch
+        //This is from Ebook
         private SerialPort  serialPort = null;
 
         private int inputSize = 0;
